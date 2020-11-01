e72a605308c2225e1b074ca6c6aa7224cfad5004
reactNoobie <reactnoobie@gmail.com>
2020-11-01 18:01:59 +0600

    Finished web-form modules

--- scripts/web-forms.js
+++ scripts/web-forms.js
@@ -6,3 +6,13 @@ output.textContent = price.value;
 price.addEventListener('input', function () {
     output.textContent = price.value;
 });
+
+const email = document.getElementById("mail");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an e-mail address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
